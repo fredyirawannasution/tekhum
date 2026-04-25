1d015c5c63187f5e6c9689925fd0c96e6838d2cd
fredyirawannasution <fredyirawannasution@gmail.com>
2026-04-25 11:24:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Contoh format: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
 window.TEKHUM_CONFIG = {
   APP_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzgpX3a_dcjqowe9V5rwvX9Z6dxM-hnVk3Xslcf5mAuS29kF-HcR4s38QGzkBW38B-S/exec',
-  APP_TITLE: 'Sistem Upload Surat KPU',
+  APP_TITLE: 'Pengelolaan Dokumen Teknis & Hukum',
   ORG_NAME: 'KPU Kabupaten Labuhanbatu Utara'
 };
