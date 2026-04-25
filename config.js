c0876f86c4ab1dc118db9415d05da497ffa52fd7
fredyirawannasution <fredyirawannasution@gmail.com>
2026-04-25 11:20:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Ganti URL ini dengan URL deployment Google Apps Script Web App Anda.
 // Contoh format: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
 window.TEKHUM_CONFIG = {
-  APP_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwYp5cJ-Hoiox4aWjig59ed-cL0cLopICIT7eRcoCGXkEW_ZBjzS2yL8gp0QPziGxgV/exec',
+  APP_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzgpX3a_dcjqowe9V5rwvX9Z6dxM-hnVk3Xslcf5mAuS29kF-HcR4s38QGzkBW38B-S/exec',
   APP_TITLE: 'Sistem Upload Surat KPU',
   ORG_NAME: 'KPU Kabupaten Labuhanbatu Utara'
 };
